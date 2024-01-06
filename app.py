8e1989bfd5bd5ff68043569709c365ccd1967019
sertemo <tejedor.moreno@gmail.com>
2024-01-06 15:05:34 +0100

    modificadas funciones en bash para ver entorno virtual activado de poetry

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 """Script con el código de la aplicación principal en Streamlit"""
+
+# librerías internas
+
+# librerías de terceros (pip install)
+import streamlit as st
+
+# librerías del proyecto
